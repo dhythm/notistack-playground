084e8513df327810a5a3396421c765d40ce53ae0
dhythm <yuta.okada.20@gmail.com>
2022-07-05 01:21:53 +0900

    check the behaviour of ClassNameGenerator

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
+import { Button } from "./components";
 
 const App: FC = () => {
-  return <div>Hello, world</div>;
+  return (
+    <>
+      <div>Hello, world</div>
+      <Button>click me</Button>
+    </>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
+import { unstable_ClassNameGenerator as ClassNameGenerator } from "@mui/material/className";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+ClassNameGenerator.configure((componentName) => `prefix-${componentName}`);
 
 ReactDOM.render(<App />, document.getElementById("root"));
